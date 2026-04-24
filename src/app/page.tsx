44df14b794a232024ef682ee8100d23d7866f190
SNM1105 <simonmedalssy@gmail.com>
2026-04-23 22:15:33 -0400

    Replace hero video with compressed boomerang file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         {/* Background media */}
         <div className="absolute inset-0 overflow-hidden">
           <HeroBoomerangVideo
-            src="/video/chabitai boomerang.mp4"
+            src="/video/chabitai_boomerang.mp4"
             poster={HERO_POSTER}
             className="h-full w-full object-cover object-center"
           />
